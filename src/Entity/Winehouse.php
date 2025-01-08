1d3ab9c9a907fbc1d0e1f61792a64d78c1ddfc65
Julien C <julien@cappiello.fr>
2025-01-09 00:07:40 +0100

    Get all winehouses

--- src/Entity/Winehouse.php
+++ src/Entity/Winehouse.php
@@ -35,17 +35,171 @@ class Winehouse
     #[ORM\Column(type: 'text', nullable: true)]
     private $description;
 
-    #[ORM\ManyToOne(targetEntity: Winemaker::class, inversedBy: 'winehouses')]
-    #[ORM\JoinColumn(nullable: false)]
-    private $winemaker;
+//    #[ORM\ManyToOne(targetEntity: Winemaker::class, inversedBy: 'winehouses')]
+//    #[ORM\JoinColumn(nullable: false)]
+//    private $winemaker;
 
-    #[ORM\OneToMany(mappedBy: 'winehouse', targetEntity: Experience::class)]
-    private $experiences;
+//    #[ORM\OneToMany(mappedBy: 'winehouse', targetEntity: Experience::class)]
+//    private $experiences;
 
-    public function __construct()
+//    public function __construct()
+//    {
+//        $this->experiences = new ArrayCollection();
+//    }
+    /**
+     * @param $name
+     * @param $address
+     * @param $city
+     * @param $region
+     * @param $country
+     * @param $zipCode
+     * @param $description
+//     * @param $winemaker
+//     * @param $experiences
+     */
+    public function __construct($name, $address, $city, $region, $country, $zipCode, $description) // $winemaker, $experiences
     {
-        $this->experiences = new ArrayCollection();
+        $this->name = $name;
+        $this->address = $address;
+        $this->city = $city;
+        $this->region = $region;
+        $this->country = $country;
+        $this->zipCode = $zipCode;
+        $this->description = $description;
+//        $this->winemaker = $winemaker;
+//        $this->experiences = $experiences;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param mixed $address
+     */
+    public function setAddress($address): void
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $city
+     */
+    public function setCity($city): void
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRegion()
+    {
+        return $this->region;
+    }
+
+    /**
+     * @param mixed $region
+     */
+    public function setRegion($region): void
+    {
+        $this->region = $region;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $country
+     */
+    public function setCountry($country): void
+    {
+        $this->country = $country;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * @param mixed $zipCode
+     */
+    public function setZipCode($zipCode): void
+    {
+        $this->zipCode = $zipCode;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription($description): void
+    {
+        $this->description = $description;
     }
 
     // Getters et setters
+    public function setPublishedAt(\DateTime $param)
+    {
+    }
 }
